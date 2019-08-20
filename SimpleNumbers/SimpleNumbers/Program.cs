8c7643a3cad9316d8be2a030cbc2eb2cc8a6b22b
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-08-20 11:11:38 +0300

    Init

---
+++ SimpleNumbers/SimpleNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
