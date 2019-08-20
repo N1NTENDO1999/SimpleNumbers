5ed21e02b766b4144fa8862a28544d76889ba50d
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-08-20 11:48:36 +0300

    Simle Number Search

--- SimpleNumbers/SimpleNumbers/Program.cs
+++ SimpleNumbers/SimpleNumbers/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace SimpleNumbers
+namespace Rextester
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (int elem in PrimeNumbers.GetPrimeNumbers(100))
+                Console.WriteLine(elem + " ");
+        }
+    }
+    class PrimeNumbers
+    {
+        public static List<int> GetPrimeNumbers(int n)
+        {
+            List<int> list = new List<int>();
+            int max = n + 1;
+            bool[] is_prime = MakeSieve(max);
+            for (int i = 2; i < max; i++)
+                if (is_prime[i])
+                    list.Add(i);
+            return list;
+        }
+        private static bool[] MakeSieve(int max)
+        {
+            bool[] is_prime = new bool[max + 1];
+            for (int i = 2; i <= max; i++) is_prime[i] = true;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (is_prime[i])
+                {
+                    for (int j = i * 2; j <= max; j += i)
+                        is_prime[j] = false;
+                }
+            }
+            return is_prime;
         }
     }
 }
